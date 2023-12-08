650f2a266a2f6b11f68d930fdf31d6591f012eaa
vane6892 <vanesam7862@gmail.com>
2023-12-08 18:46:44 -0400

    desafio 6: Plantillas de Cadena

---
+++ desafio6.js
@@ -0,0 +1,7 @@
+const Name = "Vanesa Martinez";
+const FirstName = "Maria Vanesa";
+const LastName = "Martinez Chavarria";
+const Age = 24;
+const hobby = "leer";
+
+console.log(`Mi nombre completo es ${FirstName}  ${LastName}. Tengo ${Age} años y me gusta ${hobby}.`);
